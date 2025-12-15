2019510f94431a0d134dc246cb6156390b33c370
snehav <snehavreddy12@gmail.com>
2025-12-15 09:30:43 +0530

    Creating Search Service and connecting Auction Service with Sync connection

---
+++ src/SearchService/Controllers/SearchController.cs
@@ -0,0 +1,64 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Entities;
+using SearchService.Models;
+using SearchService.RequestHelpers;
+
+namespace SearchService.Controllers;
+
+[ApiController]
+[Route("api/search")]
+public class SearchController : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<List<Item>>> SearchItems([FromQuery]SearchParams searchParams)
+    {
+        var searchTerm = searchParams.SearchTerm;
+        var pagenumber = searchParams.PageNumber;
+        var pageSize = searchParams.PageSize;
+    
+        var query = DB.PagedSearch<Item, Item>();
+
+        if(!string.IsNullOrEmpty(searchTerm))
+        {
+            query.Match(Search.Full, searchTerm).SortByTextScore();
+        } 
+
+        query = searchParams.OrderBy switch
+        {
+            "make" => query.Sort(x => x.Ascending(a => a.Make)),
+            "new" => query.Sort(x => x.Descending(a => a.CreatedAt)),
+             _ => query.Sort(x => x.Ascending(a => a.AuctionEnd)),
+        };
+
+        query = searchParams.FilterBy switch
+        {
+            "finished" => query.Match(x => x.AuctionEnd < DateTime.UtcNow),
+            "endingSoon" => query.Match(x => x.AuctionEnd < DateTime.UtcNow.AddDays(6) 
+                                        && x.AuctionEnd > DateTime.UtcNow),
+            _ => query.Match(x => x.AuctionEnd > DateTime.UtcNow)
+        };
+
+        if (!string.IsNullOrEmpty(searchParams.Seller))
+        {
+            query.Match(x => x.Seller == searchParams.Seller);
+        }
+
+        if (!string.IsNullOrEmpty(searchParams.Winner))
+        {
+            query.Match(x => x.Winner == searchParams.Winner);
+        }
+
+        query.PageNumber(pagenumber);
+        query.PageSize(pageSize);
+
+        var result = await query.ExecuteAsync();
+
+        return Ok(new
+        {
+            results = result.Results,
+            pageCount = result.PageCount,
+            totalCount = result.TotalCount,
+        });
+    }
+}
